4758400a32a3ece0b4c2b31c71a35428577144bb
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-04 14:26:57 +0000

    add api key for openweathermap

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const API_KEY = '4372892a506b377f58eb554ef7197e86';
+
 class App extends React.Component {
+
+	getWeather = async () => {
+		const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`); 
+		const data = await api_call.json();
+		console.log(data)
+	}
   render(){
     return (
       <div>
        	<Titles />
-       	<Form />
+       	<Form getWeather={this.getWeather}/>
        	<Weather />
       </div>
       );
