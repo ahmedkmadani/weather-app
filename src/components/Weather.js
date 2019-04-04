b7e484d6413aa60ef5e9c653808deaaffeb1af97
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-04 15:24:50 +0000

    view result only on submit

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,7 +5,10 @@ class Weather extends React.Component {
 	render(){
 		return (
 			<div>
-				{this.props.temperature}
+				{this.props.city && this.props.country && <p>Location: {this.props.country}, {this.props.city}</p>}
+				{this.props.temperature && <p>Temperature : {this.props.temperature}</p>}
+				{this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+				{this.props.description && <p>Condition: {this.props.description}</p>}
 			</div>
 		)
 	}
