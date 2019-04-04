33a090146f356d08f85b6b59a8e7a964413fe659
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-04-04 11:15:43 +0000

    add title component

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Titles extends React.Component {
+	render(){
+		return(
+				<div>
+					<h1>Weather FInder </h1>
+					<p>Find out temperature , condtion and more </p>
+				</div>
+			)
+	}
+};
+
+
+export default Titles;
