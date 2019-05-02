5b20fcfc3b4fa63854825e1dfc90aab8920c53fe
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-05-02 11:49:30 +0000

    change background image and colors

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Titles = () => (
 		<div>
 			<h1 className='title-container__title'>Weather Finder </h1>
-			<p className='title-container__subtitle'>Find out temperature , conidtion and more </p>
+			<p className='title-container__subtitle'>Find out temperature , condition and more </p>
 		</div>	
 	);
 
